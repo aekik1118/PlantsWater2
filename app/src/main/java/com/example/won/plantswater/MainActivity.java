7a568120b6b536dd571271fd347a5d5337c40fd6
aekik1118 <aekik1118@naver.com>
2017-11-12 12:46:45 +0900

    대략적인 틀

--- app/src/main/java/com/example/won/plantswater/MainActivity.java
+++ app/src/main/java/com/example/won/plantswater/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.won.plantswater;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String[] LIST_MENU = {"LIST1","LIST2","LIST3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,LIST_MENU);
+
+        ListView listView = (ListView) findViewById(R.id.listView1);
+        listView.setAdapter(adapter);
+
+        Button button = (Button)findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,PlantsInsert.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/won/plantswater/PlantsInsert.java
@@ -0,0 +1,13 @@
+package com.example.won.plantswater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlantsInsert extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plants_insert);
+    }
+}
