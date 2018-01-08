f7f25b7131087f33f54ffe46be4441de9134e399
Kimjongseong <kjs3870@naver.com>
2018-01-08 17:49:01 +0900

    액션바 메뉴 추가

--- app/src/main/java/com/example/won/plantswater/MainActivity.java
+++ app/src/main/java/com/example/won/plantswater/MainActivity.java
@@ -4,8 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.util.List;
@@ -21,23 +21,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,LIST_MENU);
+
 
         listView = (ListView) findViewById(R.id.listView1);
-        //listView.setAdapter(adapter);
 
-        Button button = (Button)findViewById(R.id.button4);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,PlantsInsert.class);
-                startActivity(intent);
-            }
-        });
 
         Log.d(TAG, "Oncreate()");
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.actionbar_actions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if(id == R.id.action_insert)
+        {
+            Intent intent = new Intent(MainActivity.this,PlantsInsert.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 
     protected void onStart() {
