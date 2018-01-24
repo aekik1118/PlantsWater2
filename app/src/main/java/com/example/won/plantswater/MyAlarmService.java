949e5308a890c12401bb9fe947b29c7ce416c08f
aekik1118 <aekik1118@naver.com>
2018-01-25 00:06:52 +0900

    노티피케이션 틀 제작

--- app/src/main/java/com/example/won/plantswater/MyAlarmService.java
+++ app/src/main/java/com/example/won/plantswater/MyAlarmService.java
@@ -1,7 +1,11 @@
 package com.example.won.plantswater;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
@@ -29,14 +33,38 @@ public class MyAlarmService extends Service {
             Toast.makeText(this,"에러"+intent.getIntExtra("pid",0) ,Toast.LENGTH_SHORT).show();
         else
         {
-            Toast.makeText(this,"알람"+intent.getIntExtra("pid",0) ,Toast.LENGTH_SHORT).show();
+            NotificationManager notificationManager = ( NotificationManager )getSystemService(this.NOTIFICATION_SERVICE);
+            Intent intent1 = new Intent(MyAlarmService.this,MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(MyAlarmService.this,0,intent1,PendingIntent.FLAG_UPDATE_CURRENT);
+            Notification.Builder builder = new Notification.Builder(getApplicationContext());
+            builder.setSmallIcon(R.drawable.cactus)
+                    .setContentTitle("Content Title")
+                    .setContentText("Content Text")
+                    .setContentIntent(pendingIntent)
+                    .setTicker("알림!!");
 
 
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                builder.setCategory(Notification.CATEGORY_MESSAGE)
+                        .setPriority(Notification.PRIORITY_HIGH)
+                        .setVisibility(Notification.VISIBILITY_PUBLIC);
+            }
+
+            if(Build.VERSION.SDK_INT < 16)
+            {
+                notificationManager.notify(0,builder.getNotification());
+            }
+            else
+            {
+                notificationManager.notify(0,builder.build());
+            }
+
+            //Toast.makeText(this,"알람"+intent.getIntExtra("pid",0) ,Toast.LENGTH_SHORT).show();
         }
 
-
         Log.d(TAG, "onStartCommand");
-
         return super.onStartCommand(intent, flags, startId);
     }
+
+
 }
