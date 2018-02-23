df71605fce9e837faa6010c605964d81b24ca1df
aekik1118 <aekik1118@naver.com>
2018-02-24 00:38:43 +0900

    메모리 문제 // 사진크기하고 알람에 들어가는 데이터 줄여야할듯 카메라 문제 7.0 권한 문제만 빼고 해결 식물 삭제할때 삭제하는 기능 추가해야될듯(이건 고민해봐야될듯)

--- app/src/main/java/com/example/won/plantswater/PlantsInsert.java
+++ app/src/main/java/com/example/won/plantswater/PlantsInsert.java
@@ -3,16 +3,20 @@ package com.example.won.plantswater;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.provider.Settings;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
@@ -25,10 +29,13 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class PlantsInsert extends AppCompatActivity implements View.OnClickListener{
 
@@ -37,6 +44,10 @@ public class PlantsInsert extends AppCompatActivity implements View.OnClickListe
     private static final int REQUEST_TAKE_ALBUM = 3333;
     private static final int REQUEST_IMAGE_CROP = 4444;
 
+    private static final int PICK_FROM_CAMERA = 1; //카메라 촬영으로 사진 가져오기
+    private static final int PICK_FROM_ALBUM = 2; //앨범에서 사진 가져오기
+    private static final int CROP_FROM_CAMERA = 3; //가져온 사진을 자르기 위한 변수
+
     Button btn_capture, btn_album;
     ImageView iv_view;
 
@@ -44,6 +55,70 @@ public class PlantsInsert extends AppCompatActivity implements View.OnClickListe
 
     Uri imageUri;
     Uri photoURI, albumURI;
+
+    int flag;
+
+    private String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}; //권한 설정 변수
+    private static final int MULTIPLE_PERMISSIONS = 101; //권한 동의 여부 문의 후 CallBack 함수에 쓰일 변수
+
+    private boolean checkPermissions() {
+        int result;
+        List<String> permissionList = new ArrayList<>();
+        for (String pm : permissions) {
+            result = ContextCompat.checkSelfPermission(this, pm);
+            if (result != PackageManager.PERMISSION_GRANTED) { //사용자가 해당 권한을 가지고 있지 않을 경우 리스트에 해당 권한명 추가
+                permissionList.add(pm);
+            }
+        }
+        if (!permissionList.isEmpty()) { //권한이 추가되었으면 해당 리스트가 empty가 아니므로 request 즉 권한을 요청합니다.
+            ActivityCompat.requestPermissions(this, permissionList.toArray(new String[permissionList.size()]), MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
+
+
+    //아래는 권한 요청 Callback 함수입니다. PERMISSION_GRANTED로 권한을 획득했는지 확인할 수 있습니다. 아래에서는 !=를 사용했기에
+//권한 사용에 동의를 안했을 경우를 if문으로 코딩되었습니다.
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MULTIPLE_PERMISSIONS: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < permissions.length; i++) {
+                        if (permissions[i].equals(this.permissions[0])) {
+                            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                                showNoPermissionToastAndFinish();
+                            }
+                        } else if (permissions[i].equals(this.permissions[1])) {
+                            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                                showNoPermissionToastAndFinish();
+
+                            }
+                        } else if (permissions[i].equals(this.permissions[2])) {
+                            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                                showNoPermissionToastAndFinish();
+
+                            }
+                        }
+                    }
+                } else {
+                    showNoPermissionToastAndFinish();
+                }
+                return;
+            }
+        }
+    }
+
+
+    //권한 획득에 동의를 하지 않았을 경우 아래 Toast 메세지를 띄우며 해당 Activity를 종료시킵니다.
+    private void showNoPermissionToastAndFinish() {
+        Toast.makeText(this, "권한 요청에 동의 해주셔야 이용 가능합니다. 설정에서 권한 허용 하시기 바랍니다.", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+
+
 
     @Override
     public void onClick(View view) {
@@ -58,22 +133,26 @@ public class PlantsInsert extends AppCompatActivity implements View.OnClickListe
         btn_capture = (Button) this.findViewById(R.id.button);
         btn_album = (Button) findViewById(R.id.button4);
         iv_view = (ImageView) findViewById(R.id.imageView2);
+        flag = 0;
 
         btn_capture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                captureCamera();
+                //captureCamera();
+                takePhoto();
             }
         });
 
         btn_album.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getAlbum();
+                goToAlbum();
             }
         });
 
-        checkPermission();
+        //checkPermission();
+
+        checkPermissions();
 
         //Button btn_agreeJoin = (Button) this.findViewById(R.id.button);
         Button btn_add = (Button) this.findViewById(R.id.button1);
@@ -105,10 +184,12 @@ public class PlantsInsert extends AppCompatActivity implements View.OnClickListe
                 else
                     pr = 168;
 
-                if(albumURI == null)
+                if(flag == 0)
+                {
                     MainActivity.mDatabase.insertData(et_name.getText().toString(), null, pr);
+                }
                 else
-                    MainActivity.mDatabase.insertData(et_name.getText().toString(), albumURI.toString(), pr);
+                    MainActivity.mDatabase.insertData(et_name.getText().toString(),mCurrentPhotoPath.toString(), pr);
 
                 Toast.makeText(PlantsInsert.this, "식물 추가 완료", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(PlantsInsert.this, MainActivity.class);
@@ -120,7 +201,30 @@ public class PlantsInsert extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    private void captureCamera(){
+    private void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //사진을 찍기 위하여 설정합니다.
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException e) {
+            Toast.makeText(PlantsInsert.this, "이미지 처리 오류! 다시 시도해주세요.", Toast.LENGTH_SHORT).show();              finish();
+        }
+        if (photoFile != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {//sdk 24 이상, 누가(7.0)
+                photoURI = FileProvider.getUriForFile(this, getApplication().getPackageName()+".provider", photoFile);
+            } else {//sdk 23 이하, 7.0 미만
+                photoURI = Uri.fromFile(photoFile);
+            }
+
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI); //사진을 찍어 해당 Content uri를 photoUri에 적용시키기 위함
+            startActivityForResult(intent, PICK_FROM_CAMERA);
+        }
+    }
+
+
+
+
+   /* private void captureCamera(){
         String state = Environment.getExternalStorageState();
 
         Log.d("captureCamera", "Call");
@@ -172,8 +276,8 @@ public class PlantsInsert extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(this, "저장공간이 접근 불가능한 기기입니다", Toast.LENGTH_SHORT).show();
             return;
         }
-    }
-
+    }*/
+/*
     public File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
@@ -190,16 +294,42 @@ public class PlantsInsert extends AppCompatActivity implements View.OnClickListe
         mCurrentPhotoPath = imageFile.getAbsolutePath();
 
         return imageFile;
+    }*/
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("HHmmss").format(new Date());
+        String imageFileName = "IP" + timeStamp + "_";
+        File storageDir = new File(Environment.getExternalStorageDirectory() + "/test/"); //test라는 경로에 이미지를 저장하기 위함
+        if (!storageDir.exists()) {
+            storageDir.mkdirs();
+        }
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+
+        return image;
     }
 
+    private void goToAlbum() {
 
+        Intent intent = new Intent(Intent.ACTION_PICK); //ACTION_PICK 즉 사진을 고르겠다!
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+/*
     private void getAlbum(){
         Log.i("getAlbum", "Call");
         Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
+        intent.setType("image*//*");
         intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
         startActivityForResult(intent, REQUEST_TAKE_ALBUM);
-    }
+    }*/
 
     private void galleryAddPic(){
         Log.i("galleryAddPic", "Call");
@@ -210,10 +340,131 @@ public class PlantsInsert extends AppCompatActivity implements View.OnClickListe
         mediaScanIntent.setData(contentUri);
         sendBroadcast(mediaScanIntent);
         Toast.makeText(this, "사진이 앨범에 저장되었습니다." + contentUri, Toast.LENGTH_SHORT).show();
+        flag=1;
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {
+            Toast.makeText(PlantsInsert.this, "이미지 처리 오류! 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+        }
+        if (requestCode == PICK_FROM_ALBUM) {
+            if(data==null){
+                return;
+            }
+            photoURI = data.getData();
+            cropImage();
+        } else if (requestCode == PICK_FROM_CAMERA) {
+            cropImage();
+            MediaScannerConnection.scanFile(PlantsInsert.this, //앨범에 사진을 보여주기 위해 Scan을 합니다.
+                    new String[]{photoURI.getPath()}, null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
+                        }
+                    });
+        } else if (requestCode == CROP_FROM_CAMERA) {
+            try { //저는 bitmap 형태의 이미지로 가져오기 위해 아래와 같이 작업하였으며 Thumbnail을 추출하였습니다.
+
+                if (resultCode == Activity.RESULT_OK) {
+
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoURI);
+                    Bitmap thumbImage = ThumbnailUtils.extractThumbnail(bitmap, 128, 128);
+                    ByteArrayOutputStream bs = new ByteArrayOutputStream();
+                    thumbImage.compress(Bitmap.CompressFormat.JPEG, 100, bs); //이미지가 클 경우 OutOfMemoryException 발생이 예상되어 압축
+
+                    iv_view.setImageBitmap(thumbImage);
+                    galleryAddPic();
+
+                }
+
+
+
+                //여기서는 ImageView에 setImageBitmap을 활용하여 해당 이미지에 그림을 띄우시면 됩니다.
+
+
+
+            } catch (Exception e) {
+                Log.e("ERROR", e.getMessage().toString());
+            }
+        }
+    }
+
+    public void cropImage() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            this.grantUriPermission("com.android.camera", photoURI,
+                    Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        }
+
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(photoURI, "image/*");
+
+        List<ResolveInfo> list = getPackageManager().queryIntentActivities(intent, 0);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            grantUriPermission(list.get(0).activityInfo.packageName, photoURI,
+                    Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        }
+        int size = list.size();
+        if (size == 0) {
+            Toast.makeText(this, "취소 되었습니다.", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+
+
+            Toast.makeText(this, "용량이 큰 사진의 경우 시간이 오래 걸릴 수 있습니다.", Toast.LENGTH_SHORT).show();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            }
+            intent.putExtra("crop", "true");
+            intent.putExtra("aspectX", 4);
+            intent.putExtra("aspectY", 3);
+            intent.putExtra("scale", true);
+            File croppedFileName = null;
+            try {
+                croppedFileName = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            File folder = new File(Environment.getExternalStorageDirectory() + "/test/");
+            File tempFile = new File(folder.toString(), croppedFileName.getName());
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {//sdk 24 이상, 누가(7.0)
+                photoURI = FileProvider.getUriForFile(this, getApplication().getPackageName()+".provider", tempFile);
+            } else {//sdk 23 이하, 7.0 미만
+                photoURI = Uri.fromFile(tempFile);
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            }
+
+            intent.putExtra("return-data", false);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString()); //Bitmap 형태로 받기 위해 해당 작업 진행
+
+            Intent i = new Intent(intent);
+            ResolveInfo res = list.get(0);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                i.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+                grantUriPermission(res.activityInfo.packageName, photoURI,
+                        Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            }
+            i.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+            startActivityForResult(i, CROP_FROM_CAMERA);
+
+
+        }
+
+    }
+
+
     // 카메라 전용 크랍
-    public void cropImage(){
+  /*  public void cropImage(){
         Log.i("cropImage", "Call");
         Log.i("cropImage", "photoURI : " + photoURI + " / albumURI : " + albumURI);
 
@@ -222,7 +473,7 @@ public class PlantsInsert extends AppCompatActivity implements View.OnClickListe
         // 50x50픽셀미만은 편집할 수 없다는 문구 처리 + 갤러리, 포토 둘다 호환하는 방법
         cropIntent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
         cropIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        cropIntent.setDataAndType(photoURI, "image/*");
+        cropIntent.setDataAndType(photoURI, "image*//*");
         //cropIntent.putExtra("outputX", 200); // crop한 이미지의 x축 크기, 결과물의 크기
         //cropIntent.putExtra("outputY", 200); // crop한 이미지의 y축 크기
         cropIntent.putExtra("aspectX", 1); // crop 박스의 x축 비율, 1&1이면 정사각형
@@ -233,8 +484,8 @@ public class PlantsInsert extends AppCompatActivity implements View.OnClickListe
         Log.d("crop11"," "+ albumURI);
 
         startActivityForResult(cropIntent, REQUEST_IMAGE_CROP);
-    }
-
+    }*/
+/*
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
@@ -298,7 +549,7 @@ public class PlantsInsert extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
         }
-    }
+    }*/
 
 
 
@@ -335,7 +586,7 @@ public class PlantsInsert extends AppCompatActivity implements View.OnClickListe
             }
         }
 
-        @Override
+        /*@Override
         public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
             switch (requestCode) {
                 case MY_PERMISSION_CAMERA:
@@ -350,7 +601,7 @@ public class PlantsInsert extends AppCompatActivity implements View.OnClickListe
 
                     break;
             }
-        }
+        }*/
     }
 
 
